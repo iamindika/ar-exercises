b0404eba5b516735e06941ad65d3a8683a5c17d9
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-01-07 18:12:26 +0000

    Add 3 store entries to stores table

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,13 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+puts "store name: #{store.name}, store annual revenue: #{store.annual_revenue}, store has men's apparel #{store.mens_apparel} and store has women's apparel #{store.womens_apparel}"
+
+store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+puts "store name: #{store.name}, store annual revenue: #{store.annual_revenue}, store has men's apparel #{store.mens_apparel} and store has women's apparel #{store.womens_apparel}"
+
+store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+puts "store name: #{store.name}, store annual revenue: #{store.annual_revenue}, store has men's apparel #{store.mens_apparel} and store has women's apparel #{store.womens_apparel}"
+
+puts "Number of stores: #{Store.count}"
