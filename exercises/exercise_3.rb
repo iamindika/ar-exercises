9eabc36367978128f5a9eba270a7fbd9b114ac13
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-01-07 18:23:18 +0000

    Delete store id: 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,7 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+puts "store name: #{@store3.name} and store annual revenue: #{@store3.annual_revenue}"
+@store3.destroy
+puts "Number of stores: #{Store.count}"
